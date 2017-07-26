33d0c9ebc40d230cc713bd333147d61d35b748b8
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-26 08:12:27 +0300

    this commit updates CRUD functionality of Stores

--- app.rb
+++ app.rb
@@ -9,12 +9,51 @@ get("/") do
   @brands = Brand.all()
   erb(:index)
 end
-post("/stores") do
+post("/stores/new") do
   name = params.fetch("name")
   store = Store.new("name"=>name)
-  if store.save.to_scd
+  if store.save
     erb(:success)
 else
   erb(:error)
+  end
+end
+
+get("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store)
+end
+post("/brands/new") do
+  name = params.fetch("name")
+  @brand = Brand.new("name" => name)
+  if @brand.save()
+    erb(:success)
+  else
+    erb(:error)
+  end
+end
+get("/brands/:id") do
+  @brand = Brand.find(params.fetch("id").to_i())
+  erb(:store)
+end
+get("/stores/:id/edit")do
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store_edit)
+end
+patch("/stores/:id") do
+  @stores = Store.all()
+  name = params.fetch("name")
+  @store = Store.find(params.fetch("id").to_i())
+  if   @store.update("name"=>name)
+    erb(:success)
+  else
+    erb(:error)
+  end
+end
+delete("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  @store = Store.all()
+  if  @store.delete()
+    erb(:success)
   end
 end
