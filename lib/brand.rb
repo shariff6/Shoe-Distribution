cbcf4c815adc818b71ef13f7b31a48a04ec0ba87
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-26 09:29:21 +0300

    this commit adds validations

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ActiveRecord::Base
   has_many :distributions
   has_many :stores, through: :distributions
+  validates(:name, :presence => true)
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :distributions
   has_many :brands, through: :distributions
+  validates(:name, :presence => true)
+
 end
